b73572b3f6cdc2cb1c284298f7c1c65f18e8ca99
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2022-07-31 17:56:17 -0400

    first commit
    wip

---
+++ react_todos/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    console.log(newItem);
+    console.log(items);
+    if (!newItem) {
+      alert("Ingresa un elemento.")
+      return; 
+    }
+
+    const item = {
+      id: Math.floor(Math.random() *1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+
+    setNewItem("");
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id != id);
+    setItems(newArray);
+  }
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <input
+        type="text"
+        placeholder='Agrega un elemento'
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+      />
+      
+      <button onClick={() => addItem()}>Add</button>
+
+      <ul>
+        {items.map(item => {
+          return(
+            <li key={item.id}>{item.value} <button className='delete-button' onClick={ () => deleteItem(item.id)}>❌</button> </li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
